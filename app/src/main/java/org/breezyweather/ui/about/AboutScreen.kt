008ddf704d5c9746498255fb44acff1f63a5ea33
vomw <22770640+vomw@users.noreply.github.com>
2026-02-08 09:11:25 +0800

    remove

--- app/src/main/java/org/breezyweather/ui/about/AboutScreen.kt
+++ app/src/main/java/org/breezyweather/ui/about/AboutScreen.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
